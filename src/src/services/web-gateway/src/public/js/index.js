8b639976a4e5d43346f70e8046a8e635b48bb39a
Alex Kirtley-Close <alex.kirtley-close@grafana.com>
2023-02-20 20:58:15 +0000

    Added better faro tracing

--- src/src/services/web-gateway/src/public/js/index.js
+++ src/src/services/web-gateway/src/public/js/index.js
@@ -5,21 +5,30 @@ import {
   ReactIntegration,
   ReactRouterVersion
 } from '@grafana/faro-react';
-import { TracingInstrumentation } from '@grafana/faro-web-tracing';
+import { Resource } from '@opentelemetry/resources';
+import { SemanticResourceAttributes } from '@opentelemetry/semantic-conventions';
+import { WebTracerProvider } from '@opentelemetry/sdk-trace-web';
+import { BatchSpanProcessor } from '@opentelemetry/sdk-trace-base';
+import { W3CTraceContextPropagator } from '@opentelemetry/core';
+import { ZoneContextManager } from '@opentelemetry/context-zone';
+
+import { TracingInstrumentation, FaroSessionSpanProcessor, FaroTraceExporter } from '@grafana/faro-web-tracing';
 import { createBrowserHistory } from 'history';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Route } from 'react-router-dom';
 import App from './components/app';
 
-const VERSION = "3.4.23";
+const VERSION = "3.4.26";
 const NAME = 'ecommerce-frontend';
 const COLLECTOR_URL = "http://35.224.5.38:12121/collect";
 
 const history = createBrowserHistory();
 
+console.log(process.env.FARO_ENDPOINT)
+
 // Initialize Faro
-initializeFaro({
+const faro = initializeFaro({
   url: COLLECTOR_URL,
   apiKey: 'secret',
   instrumentations: [
@@ -45,6 +54,23 @@ initializeFaro({
     version: VERSION,
     environment: 'prod'
   }
+});
+
+// set up otel
+const resource = Resource.default().merge(
+  new Resource({
+    [SemanticResourceAttributes.SERVICE_NAME]: NAME,
+    [SemanticResourceAttributes.SERVICE_VERSION]: VERSION,
+  })
+);
+
+const provider = new WebTracerProvider({ resource });
+
+provider.addSpanProcessor(new FaroSessionSpanProcessor(new BatchSpanProcessor(new FaroTraceExporter({ faro }))));
+
+provider.register({
+  propagator: new W3CTraceContextPropagator(),
+  contextManager: new ZoneContextManager(),
 });
 
 ReactDOM.render(
