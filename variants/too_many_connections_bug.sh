e499a875c8d96f789c8f73066b2ed26aa28e2fdd
Alex Kirtley-Close <alex.kirtley-close@grafana.com>
2023-02-23 15:37:29 +0000

    tidied too many connections bug

--- variants/too_many_connections_bug.sh
+++ variants/too_many_connections_bug.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
-kubectl scale --replicas=0 deploy/k6
+kubectl exec --namespace=mltdemo deploy/product-data -- psql --username "user3" postgres -c "SELECT pg_terminate_backend(pid) FROM pg_stat_activity WHERE usename = 'user2';"
 
-kubectl exec deploy/product-data -- psql --username "user3" postgres -c "SELECT pg_terminate_backend(pid) FROM pg_stat_activity WHERE usename = 'user2';"
-
-kubectl exec deploy/product-data -- psql --username "user3" postgres -c "SELECT pg_sleep(240);" &
-kubectl exec deploy/product-data -- psql --username "user4" postgres -c "SELECT pg_sleep(240);" &
-kubectl exec deploy/product-data -- psql --username "user5" postgres -c "SELECT pg_sleep(240);" &
-kubectl exec deploy/product-data -- psql --username "user6" postgres -c "SELECT pg_sleep(240);" &
-kubectl exec deploy/product-data -- psql --username "user7" postgres -c "SELECT pg_sleep(240);" &
-
-kubectl scale --replicas=1 deploy/k6
+kubectl exec --namespace=mltdemo deploy/product-data -- psql --username "user3" postgres -c "SELECT pg_sleep(240);" &
+kubectl exec --namespace=mltdemo deploy/product-data -- psql --username "user4" postgres -c "SELECT pg_sleep(240);" &
+kubectl exec --namespace=mltdemo deploy/product-data -- psql --username "user5" postgres -c "SELECT pg_sleep(240);" &
+kubectl exec --namespace=mltdemo deploy/product-data -- psql --username "user6" postgres -c "SELECT pg_sleep(240);" &
+kubectl exec --namespace=mltdemo deploy/product-data -- psql --username "user7" postgres -c "SELECT pg_sleep(240);" &
